0e44189a6a47e642f91c4c38c2e59004bdb18b32
EternalWang <553921586@qq.com>
2016-12-11 02:40:02 +0800

    Revert "Revert "合并以及解决BUG""
    This reverts commit 8b51eea50e0f3f775727b76332576d210f4f8d68.

--- build-WordCloud-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_mysyntaxhighlighter.cpp
+++ build-WordCloud-Desktop_Qt_5_7_0_MinGW_32bit-Debug/debug/moc_mysyntaxhighlighter.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'mainwidget.h'
+** Meta object code from reading C++ file 'mysyntaxhighlighter.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../WordCloud/mainwidget.h"
+#include "../../WordCloud(QT5)/mysyntaxhighlighter.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'mainwidget.h' doesn't include <QObject>."
+#error "The header file 'mysyntaxhighlighter.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.7.0. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -18,25 +18,25 @@
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
-struct qt_meta_stringdata_MainWidget_t {
+struct qt_meta_stringdata_MySyntaxHighlighter_t {
     QByteArrayData data[1];
-    char stringdata0[11];
+    char stringdata0[20];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_MainWidget_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_MySyntaxHighlighter_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_MainWidget_t qt_meta_stringdata_MainWidget = {
+static const qt_meta_stringdata_MySyntaxHighlighter_t qt_meta_stringdata_MySyntaxHighlighter = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "MainWidget"
+QT_MOC_LITERAL(0, 0, 19) // "MySyntaxHighlighter"
 
     },
-    "MainWidget"
+    "MySyntaxHighlighter"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_MainWidget[] = {
+static const uint qt_meta_data_MySyntaxHighlighter[] = {
 
  // content:
        7,       // revision
@@ -52,7 +52,7 @@ static const uint qt_meta_data_MainWidget[] = {
        0        // eod
 };
 
-void MainWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void MySyntaxHighlighter::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     Q_UNUSED(_o);
     Q_UNUSED(_id);
@@ -60,28 +60,28 @@ void MainWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     Q_UNUSED(_a);
 }
 
-const QMetaObject MainWidget::staticMetaObject = {
-    { &QWidget::staticMetaObject, qt_meta_stringdata_MainWidget.data,
-      qt_meta_data_MainWidget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+const QMetaObject MySyntaxHighlighter::staticMetaObject = {
+    { &QSyntaxHighlighter::staticMetaObject, qt_meta_stringdata_MySyntaxHighlighter.data,
+      qt_meta_data_MySyntaxHighlighter,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
-const QMetaObject *MainWidget::metaObject() const
+const QMetaObject *MySyntaxHighlighter::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *MainWidget::qt_metacast(const char *_clname)
+void *MySyntaxHighlighter::qt_metacast(const char *_clname)
 {
     if (!_clname) return Q_NULLPTR;
-    if (!strcmp(_clname, qt_meta_stringdata_MainWidget.stringdata0))
-        return static_cast<void*>(const_cast< MainWidget*>(this));
-    return QWidget::qt_metacast(_clname);
+    if (!strcmp(_clname, qt_meta_stringdata_MySyntaxHighlighter.stringdata0))
+        return static_cast<void*>(const_cast< MySyntaxHighlighter*>(this));
+    return QSyntaxHighlighter::qt_metacast(_clname);
 }
 
-int MainWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int MySyntaxHighlighter::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = QWidget::qt_metacall(_c, _id, _a);
+    _id = QSyntaxHighlighter::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     return _id;
